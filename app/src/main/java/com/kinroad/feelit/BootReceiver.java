f05354276759c9d41daa3af3100a3cb1f9db6c25
kinroadzxc <495571156@qq.com>
2015-10-02 17:48:08 +0800

    MainService now can start automatically when Boot_Complete

---
+++ app/src/main/java/com/kinroad/feelit/BootReceiver.java
@@ -0,0 +1,14 @@
+package com.kinroad.feelit;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, MainService.class);
+        context.startService(i);
+    }
+}
